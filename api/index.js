85beb7611409bd8b19a8e1e4ccf9f91a0ad258e4
Shaik-Amreen <69184237+Shaik-Amreen@users.noreply.github.com>
2022-08-05 17:44:44 +0530

    backend

---
+++ api/index.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: "10000mb", extended: true }))
+
+app.use(cors())
+
+
+app.use(function (req, res, next) {
+    res.setHeader("Content-Security-Policy", "frame-ancestors 'self';");
+    next();
+});
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    next()
+})
+
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+
+
+
+const host = '0.0.0.0';
+const port = process.env.PORT || 4000;
+
+app.listen(port, host, () => console.log("server listened" + port + host))
