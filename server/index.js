e8eb82c8a3d1c94d2332cc224efca7f391b2532f
Shaik-Amreen <69184237+Shaik-Amreen@users.noreply.github.com>
2022-08-30 23:43:43 +0530

    deploys\

--- server/index.js
+++ server/index.js
@@ -25,10 +25,17 @@ app.use(function (req, res, next) {
 })
 
 
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static('client/build'))
+    const path = require("path")
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-})
+
+
+
 
 
 
